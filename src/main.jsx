efdd368efef576b149c5f27a621cb9f2ab8ef079
SebastiaAVi <se.avilesv@duocuc.cl>
2026-01-30 14:13:55 -0300

    paginas iniciales

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+// src/main.jsx (versión con react-router)
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'   // ← Importante: agregar esto
 import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <BrowserRouter>           {/* ← Envuelve toda la app aquí */}
+      <App />
+    </BrowserRouter>
+
 )
